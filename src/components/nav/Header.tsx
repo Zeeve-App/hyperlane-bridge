c54cbec04ee6b53d1b0c7e5a3e684d4c3fd683e3
karan.sagar <kara.sagar30@zeeve.io>
2025-09-25 13:30:42 +0530

    update-header

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -3,21 +3,30 @@ import Link from 'next/link';
 import { ConnectWalletButton } from '../../features/wallet/ConnectWalletButton';
 import Logo from '../../images/logos/app-logo.svg';
 import Name from '../../images/logos/app-name.svg';
-import Title from '../../images/logos/app-title.svg';
+// import Title from '../../images/logos/app-title.svg';
+import Brand from '../../images/logos/Dark.webp';
+
 
 export function Header() {
   return (
     <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
       <div className="flex items-start justify-between">
         <Link href="/" className="flex items-center py-2">
+          <Image src={Brand} width={24} alt="" className="ml-2 mt-0.5 pb-px  border-r border-gray-200" />
           <Image src={Logo} width={24} alt="" />
           <Image src={Name} width={130} alt="" className="ml-2 mt-0.5 hidden sm:block" />
-          <Image src={Title} width={210} alt="" className="ml-2 mt-0.5 pb-px" />
+          <h1 className="ml-2 pt-px text-2xl font-bold uppercase tracking-wide text-white">PandaC</h1>
+          {/* <Image src={Title} width={210} alt="" className="ml-2 mt-0.5 pb-px" /> */}
         </Link>
         <div className="flex flex-col items-end gap-2 md:flex-row-reverse md:items-start">
           <ConnectWalletButton />
         </div>
       </div>
+      {/* <div className="flex items-start justify-left pt-1"> */}
+        {/* <Link href="/" className="flex items-center py-2"> */}
+          {/* <Image src={Brand} width={130} alt="" className="ml-2 mt-0.5 pb-px " /> */}
+        {/* </Link> */}
+      {/* </div> */}
     </header>
   );
 }
--- src/consts/app.ts
+++ src/consts/app.ts
@@ -11,6 +11,6 @@ export const APP_NAME = 'Zeeve-hyperlane Bridge';
 export const APP_DESCRIPTION = 'A DApp for Hyperlane Warp Route transfers';
 export const APP_URL = '#';
 export const BRAND_COLOR = Color.primary['500'];
-export const BACKGROUND_COLOR = Color.gray['500'];
+export const BACKGROUND_COLOR = Color.red['500'];
 // export const BACKGROUND_IMAGE = 'url(/backgrounds/main.svg)';
 export const BACKGROUND_IMAGE = '';
