62262da80ab171c26c49929ee83b09d84bc0361a
karan.sagar <kara.sagar30@zeeve.io>
2025-09-25 13:52:14 +0530

    update-layout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -35,7 +35,7 @@ export function AppLayout({ children }: PropsWithChildren) {
           <main className="my-4 flex w-full flex-1 items-center justify-center">{children}</main>
         </div>
         <div>
-          <div className="mt-14 w-full py-4 text-center text-sm text-theme-primary-content shadow-lg">Cogitus by Zeeve © 2025</div>
+          <div className="mt-4 w-full py-4 text-center text-md text-theme-primary-content shadow-lg">Cogitus by Zeeve © 2025</div>
         </div>
         {/* <Footer /> */}
       </div>
--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -12,10 +12,11 @@ export function Header() {
     <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
       <div className="flex items-start justify-between">
         <Link href="/" className="flex items-center py-2">
-          <Image src={Brand} width={24} alt="" className="ml-2 mt-0.5 pb-px  border-r border-gray-200" />
+          <Image src={Brand} width={50} alt="" className=" mt-0.5 pb-px  border-r border-gray-200" />
+          <div className="h-6 w-px bg-gray-200 mx-2"></div>
           <Image src={Logo} width={24} alt="" />
           <Image src={Name} width={130} alt="" className="ml-2 mt-0.5 hidden sm:block" />
-          <h1 className="ml-2 pt-px text-2xl font-bold uppercase tracking-wide text-white">PandaC</h1>
+          <h1 className="ml-2 pt-px text-2xl font-bold uppercase tracking-wide text-white">Panda C</h1>
           {/* <Image src={Title} width={210} alt="" className="ml-2 mt-0.5 pb-px" /> */}
         </Link>
         <div className="flex flex-col items-end gap-2 md:flex-row-reverse md:items-start">
