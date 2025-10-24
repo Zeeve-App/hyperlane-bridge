bec21117a228b690d964af788a16d08e34f9786d
karan.sagar <kara.sagar30@zeeve.io>
2025-10-24 13:07:09 +0530

    fix-google-font

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const cspHeader = `
   script-src 'self' 'wasm-unsafe-eval';
   connect-src *;
   img-src 'self' blob: data: ${IMG_SRC_HOSTS.join(' ')};
-  font-src 'self' data: https://fonts.gstatic.com;
+  font-src 'self' data: https://fonts.gstatic.com https://fonts.googleapis.com;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
